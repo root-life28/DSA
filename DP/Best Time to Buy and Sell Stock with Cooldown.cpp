16acbd7303cfe92c3adbf1dc9910cb608695eb84
Muzahidul <bekon28@outlook.com>
2022-11-18 11:15:02 +0600

    bottom up

--- DP/Best Time to Buy and Sell Stock with Cooldown.cpp
+++ DP/Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+//recursive
  int solve(vector<int>&prices,int i,int n,int buy,vector<vector<int>>&dp)
  {
     if(i>=n) return 0;
@@ -22,5 +23,28 @@ int main()
     vector<int>prices={1,2,3,0,2};
     int n=prices.size();
     vector<vector<int>>dp(n+1,vector<int>(2,-1));
-    cout<<solve(prices,0,n,1,dp);
+    cout<<solve(prices,0,n,1,dp)<<endl;
+
+
+// bottom -up
+    vector<vector<int>>dp1(n+2,vector<int>(2,0));
+    for (int i =n-1; i>=0; i--)
+    {
+        for (int buy = 0; buy <=1; buy++)
+        {
+             int profit =0;
+            if(buy)
+              profit =max(-prices[i]+dp1[i+1][0],0+dp1[i+1][1]);
+            else{
+                profit=max(prices[i]+dp1[i+2][1],0+dp1[i+1][0]);
+            }
+
+            dp1[i][buy]= profit;
+        }
+        
+    }
+
+    cout<<dp1[0][1]<<endl;
+
+    
 }
