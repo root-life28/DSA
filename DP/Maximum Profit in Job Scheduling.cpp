8f20b7508436ab84f23c209e140276a1ede452a8
Muzahidul <bekon28@outlook.com>
2022-11-29 11:18:07 +0600

    Maximum Profit in Job Scheduling

--- DP/Maximum Profit in Job Scheduling.cpp
+++ DP/Maximum Profit in Job Scheduling.cpp
@@ -7,21 +7,16 @@ struct  Profit{
 };
 
 
-int solve(vector<struct Profit>&st,int i,int end,vector<int>&dp)
+int solve(vector<struct Profit>&st,int i,int end,vector<int>&dp,vector<int>&sta)
 {
     if(i==st.size()) return 0;
     if(dp[i]!=0) return dp[i];
-
     int a=0,b=0;
-    if(st[i].sT<end)
-    {
-        return  0+solve(st,i+1,end,dp);
-    }
-  else {
-        a=st[i].P+solve(st,i+1,st[i].eT,dp);
-        b=0+solve(st,i+1,end,dp);
-        }
-    
+    // find next position
+    auto idx=lower_bound(sta.begin(),sta.end(),st[i].eT)-sta.begin();
+
+        a=st[i].P+solve(st,idx,st[i].eT,dp,sta);
+        b=0+solve(st,i+1,end,dp,sta);    
 
 
     return dp[i]= max(a,b);
@@ -29,22 +24,28 @@ int solve(vector<struct Profit>&st,int i,int end,vector<int>&dp)
 }
 int main()
 {
-    vector<int>startTime={4,2,4,8,2};
+    vector<int>staTime={4,2,4,8,2};
     vector<int>endTime={5,5,5,10,8};
     vector<int>profit={1,2,8,10,4};
     
-    
-    vector<int>dp(startTime.size()+1,0);
-    vector<struct Profit>st(startTime.size());
-    for (int i = 0; i <startTime.size() ; i++)
+    vector<int>dp(staTime.size()+1,0);
+    vector<struct Profit>st(staTime.size());
+    for (int i = 0; i <staTime.size() ; i++)
     {
         st[i].eT=endTime[i];
-        st[i].sT=startTime[i];
+        st[i].sT=staTime[i];
         st[i].P=profit[i];
     }
 
      sort(st.begin(),st.end(),[](const Profit &a, const Profit &b){return a.sT<b.sT;});
-      cout<<solve(st,0,0,dp)<<endl;
+     vector<int>sta;
+     for (int i = 0; i < st.size(); i++)
+     {
+        sta.push_back(st[i].sT);
+     }
+
+     
+      cout<<solve(st,0,0,dp,sta)<<endl;
 
  
   
