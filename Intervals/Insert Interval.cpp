a4dcaf6d9efa4c1f8849e2e395c4e22632e8734d
Muzahidul <bekon28@outlook.com>
2023-01-17 13:25:53 +0600

    Insert Interval

--- Intervals/Insert Interval.cpp
+++ Intervals/Insert Interval.cpp
@@ -3,8 +3,25 @@ using namespace std;
 
 int main()
 {
-    vector<vector<int>>intervals={{1,3},{6,9}};
+    vector<vector<int>>intervals={{1,3},{6,9},{11,12}};
     vector<int> newInterval={2,5};
+    vector<vector<int>>ans;
+  int i=0;
+  int n=intervals.size();
+
+        while (i < n && intervals[i][1] < newInterval[0]) ans.push_back(intervals[i++]);
+        while (i < n && intervals[i][0] <= newInterval[1]) newInterval = {min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])}, i++;
+        ans.push_back(newInterval);
+        while (i < n) ans.push_back(intervals[i++]);
+  
+
+  
+  
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout<<ans[i][0]<<" "<<ans[i][1]<<" "<<endl;
+    }
+    
 
     
     return 0;
